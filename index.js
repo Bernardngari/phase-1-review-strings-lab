8457f2bbd02da25a9b15bfb10b969c9a6fcd76e3
Bernardngari <bernardngari.ke@gmail.com>
2022-06-06 16:01:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Bernard maina";
+const welcomeMessage = "Welcome to Flatbook, " + " " + "Bernard maina!";
+let userCaps = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, " ${ userCaps }!`;
+
+let initial = currentUser[0];
+const shortGreeting = `Welcome, ${initial}!`
